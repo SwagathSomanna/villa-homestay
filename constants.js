87dcadb817ffe31b6cd836d5215daced160d9539
IamUsike <sathwikthanmay4@gmail.com>
2026-01-25 21:52:53 +0530

    feat: Partial booking controller
    room verification partial implementation

--- constants.js
+++ constants.js
@@ -1,3 +1,5 @@
 export const VILLA_NAME = "Anudina Kuteera";
 export const PORT = 4000;
 export const TARGET_TYPE = ["floor", "villa", "room"];
+export const FLOORS = ["F1", "F2"];
+export const ROOMS = ["R1", "R2"];
--- controllers/booking.controller.js
+++ controllers/booking.controller.js
@@ -2,6 +2,7 @@ import { Booking } from "../models/booking.model.js";
 import crypto from "crypto";
 import { VILLA_NAME, TARGET_TYPE } from "../constants.js";
 
+//this is done so that all checks are done only on date basis
 function parseDateOnly(str) {
   const [y, m, d] = str.split("-").map(Number);
   return new Date(y, m - 1, d); // local midnight
@@ -15,9 +16,66 @@ function parseDateOnly(str) {
 
 async function verifyRoomStatus(roomInfo) {
   if (!roomInfo.targetType || !TARGET_TYPE.includes(roomInfo.targetType)) {
-    return res.status(400).json({ message: "Please select a valid entry " });
+    res.status(400).json({ message: "Please select a valid entry " });
+    return 0;
   }
-  const existingCheckin = model.find();
+
+  if (roomInfo.targetType === "floor") {
+    const floorSelection = await Booking.findOne({
+      targetType: "floor",
+      floodId: roomInfo?.floorId,
+    });
+
+    if (!floorSelection) {
+      res.status(400).json({ message: "Please select a valid floor" });
+      return 0;
+    }
+  }
+
+  if (roomInfo.targetType === "room") {
+    const roomSelection = await Booking.findOne({
+      targetType: "room",
+      floorId: roomInfo?.roomId,
+    });
+
+    if (!roomSelection) {
+      res.status(400).json({ message: "Please select a valid room" });
+      return 0;
+    }
+  }
+
+  if (roomInfo.targetType === "villa") {
+    //check if two bookings overlap
+    const curMaxCheckout = Booking.find({
+      checkOut: { $gt: roomInfo.checkOut },
+    });
+    const curMinCheckout = Booking.find({
+      checkIn: { $lt: roomInfo.checkIn },
+    });
+
+    //cos model.find invariably returns an array. yea yea i could've used findone
+    if (!curMaxCheckout.length || !curMinCheckout.length) {
+      res
+        .status(400)
+        .json({ message: "Villa is not available for the selected dates" });
+      return 0;
+    }
+  }
+
+  //for f1 - check f1, r1, r2
+  else if (roomInfo.targetType === "floor") {
+    const roomsInFloor =
+      roomInfo.floorId === "F1"
+        ? [{ roomId: "R1" }, { roomId: "R2" }]
+        : [{ roomId: "R3" }, { roomId: "R4" }];
+
+    const curMaxCheckoutFloor = Booking.find({
+      targetType: floor,
+      floorId: roomInfo.floorId,
+      $or: [...roomsInFloor],
+      checkOut: { $gt: roomInfo.checkOut },
+    });
+  }
 }
 
 export const bookVilla = async (req, res) => {
