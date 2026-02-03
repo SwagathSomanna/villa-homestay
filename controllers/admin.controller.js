412b6eb9c3afe55ff9bb501b42fcba164069e4ee
IamUsike <sathwikthanmay4@gmail.com>
2026-02-03 13:15:28 +0530

    admin login

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -19,12 +19,29 @@ const generateAccessToken = (res) => {
 
 export const login = (req, res) => {
   try {
+    const adminName = process.env.ADMIN_USERNAME;
+    const adminPassword = process.env.ADMIN_PASSWORD;
     const { username, password } = req.body;
     if (!username || !password) {
       return res
         .status(400)
         .json({ message: "Both username and password are required" });
     }
+
+    if (username !== adminName || password !== adminPassword) {
+      return res.status(401).json({ message: "Incorrect Credentials" });
+    }
+
+    const accessToken = generateAccessToken();
+    const options = {
+      httpOnly: true,
+      secure: true,
+    };
+
+    return res
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .json({ message: "Login Successful" });
   } catch (error) {
     return res.json(500).json({ message: "something went wrong" });
   }
--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { verifyJWT } from "../middleware/admin.midddleware.js";
+import { login } from "../controllers/admin.controller.js";
 
 const router = Router();
+
+router.post("/login", login);
 
 export default router;
