a67dba8c9aa36acbcf0c06782a71bab4b4fd1518
IamUsike <sathwikthanmay4@gmail.com>
2026-02-15 13:27:25 +0530

    :tear

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:4000",
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
   }),
 );
